8852b9eb9292e5c70d22f3d13e7d818a927927b8
PhireRed2015 <davkim1030@gmail.com>
2019-11-13 11:08:06 +0900

    SplashActivity Created - 뷰, 자바 추가 - Thread로 자동으로 Tutorial로 Intent

---
+++ app/src/main/java/com/phirered2015/homedoctor/activity/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.phirered2015.homedoctor.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.phirered2015.homedoctor.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private Handler mHandler;
+    private Runnable mRunnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+
+        // MainActivity.class 자리에 다음에 넘어갈 액티비티를 넣어주기
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(getApplicationContext(), TutorialActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        // Handler를 통한 대기시간 설정
+        // (임의로 출력 확인을 위해 적은것 뿐, 원래는 앱 로딩 끝나면 바로 넘어가게 되어있음)
+        mHandler = new Handler();
+        // mRunnable 내부 run() 실행하려면 기다려야 하는 delayMillis
+        mHandler.postDelayed(mRunnable, 3000);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //초반 플래시 화면에서 넘어갈때 뒤로가기 버튼 못누르게 함
+    }
+}
