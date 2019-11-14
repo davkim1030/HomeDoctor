b6179654a3c0e8e37c3c912a4a2613c63681ace0
PhireRed2015 <davkim1030@gmail.com>
2019-11-14 09:19:55 +0900

    LoginActivity Created

---
+++ app/src/main/java/com/phirered2015/homedoctor/activity/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.phirered2015.homedoctor.activity;
+
+import android.os.Bundle;
+
+import com.phirered2015.homedoctor.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceStated){
+        super.onCreate(savedInstanceStated);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
