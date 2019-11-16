a7fb3e431a4843549629b7aff520fab6189278c7
MKSeo <modori007@naver.com>
2019-11-16 18:51:47 +0900

    Main activity; drawer created

--- app/src/main/java/com/phirered2015/homedoctor/activity/MainActivity.java
+++ app/src/main/java/com/phirered2015/homedoctor/activity/MainActivity.java
@@ -1,18 +1,28 @@
 package com.phirered2015.homedoctor.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.navigation.NavigationView;
 import com.phirered2015.homedoctor.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 }
