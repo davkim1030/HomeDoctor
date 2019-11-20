9d9815c7da468acb05f2eaf2a92a6f6d68b3a306
MKSeo <modori007@naver.com>
2019-11-21 08:10:59 +0900

    drawer activity import declaration changed

--- app/src/main/java/com/phirered2015/homedoctor/activity/MainActivity.java
+++ app/src/main/java/com/phirered2015/homedoctor/activity/MainActivity.java
@@ -1,9 +1,6 @@
 package com.phirered2015.homedoctor.activity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -12,9 +9,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 
-import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.navigation.NavigationView;
 import com.phirered2015.homedoctor.R;
 
@@ -25,18 +20,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setCustomView(R.layout.app_bar_main);
-        DrawerLayout drawer = findViewById(R.id.drawerLayout);
-        NavigationView nv = findViewById(R.id.nav_view);
+
+        DrawerLayout drawer = findViewById(R.id.drawerLayout); // drawer 정보
+        NavigationView nv = findViewById(R.id.nav_view); // navigation 정보
         mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_mypage, R.id.nav_basket, R.id.nav_delivery, R.id.nav_booking).setDrawerLayout(drawer).build();
         NavController nc = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, nc, mAppBarConfiguration);
         NavigationUI.setupWithNavController(nv, nc);
     }
+    /*
+    onCreateOptionsMenu
+    메뉴 확장
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    /*
+    onSupportNavigateUp
+    navigation flow 관리
+     */
     @Override
     public  boolean onSupportNavigateUp() {
          NavController nc = Navigation.findNavController(this, R.id.nav_host_fragment);
